41617c169264e82e74f33752a974f5a5298976d8
adolesamuel <adolesamuel4@gmail.com>
2021-01-11 15:12:15 +0100

    Card used as listtile

--- lib/home/task_page/task_list_tile.dart
+++ lib/home/task_page/task_list_tile.dart
@@ -6,20 +6,42 @@ import 'package:provider/provider.dart';
 class TaskListTile extends StatelessWidget {
   final Task task;
   final VoidCallback onTap;
+  final VoidCallback onEditTap;
+  final VoidCallback onDeleteTap;
+  final VoidCallback onSwitchTap;
 
-  const TaskListTile({Key key, this.task, this.onTap}) : super(key: key);
+  const TaskListTile(
+      {Key key,
+      this.task,
+      this.onTap,
+      this.onEditTap,
+      this.onDeleteTap,
+      this.onSwitchTap})
+      : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     final database = Provider.of<Database>(context);
-    return SwitchListTile(
-      title: Text(task.taskName),
-      subtitle: Text(
-          '${task.taskComment} : ${task.startTime.hour}:${task.startTime.minute}'),
-      value: task.active,
-      onChanged: (bool value) {
-        database.setActive(task, value);
-      },
+    return Card(
+      child: Column(
+        children: [
+          SwitchListTile(
+            title: Text(task.taskName),
+            subtitle: Text(
+                '${task.taskComment} : ${task.startTime.hour}:${task.startTime.minute}'),
+            value: task.active,
+            onChanged: (bool value) {
+              database.setActive(task, value);
+            },
+          ),
+          Row(
+            children: [
+              IconButton(icon: Icon(Icons.edit), onPressed: onEditTap),
+              IconButton(icon: Icon(Icons.delete), onPressed: onDeleteTap),
+            ],
+          )
+        ],
+      ),
     );
   }
 }
